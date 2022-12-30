70a16f103383a10cccff9c53d9339794c6646b97
Roberto López <ouphenus.developer@gmail.com>
2022-12-29 22:55:03 -0500

    Ordenando clases, exponiendo las imágenes para seteo externo

---
+++ bin/main.js
@@ -0,0 +1,8 @@
+import { OuphenusStarField } from "./ouphenus-starfield.js";
+// Creando el Campo de estrellas
+let starfield = new OuphenusStarField.Main();
+// Seteando las imágenes para las estrellas
+OuphenusStarField.Star.setupStarsImages(new Array('images/star_white.png', 'images/star_red.png', 'images/star_blue.png', 'images/star_yellow.png'));
+// Añadiendo Banner StartWars (solo de ejemplo)
+starfield.createBanner();
+//# sourceMappingURL=main.js.map
